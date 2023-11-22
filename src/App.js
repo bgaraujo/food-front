4650d30cc0251c3191fa6b5113d1cc9a73d5df06
Bruno <baraujo.dev@gmail.com>
2023-11-22 16:10:43 -0300

    correção no router

--- src/App.js
+++ src/App.js
@@ -2,7 +2,9 @@ import './App.css';
 import * as React from "react";
 import {
   createBrowserRouter,
-  RouterProvider,
+  Route,
+  Routes,
+  BrowserRouter
 } from "react-router-dom";
 import SimpleBottomNavigation from "./components/footer"
 import HomeAppBar from "./components/AppBar"
@@ -12,38 +14,26 @@ import Home from "./routes/Home";
 import ErrorPage from "./routes/error-page";
 import Store from "./routes/Store";
 
-const router = createBrowserRouter([
-  {
-    path: "/",
-    element: <Home />,
-    errorElement: <ErrorPage />,
-  },
-  {
-    path: "/home",
-    element: <Home />,
-  },
-  {
-    path: "/store/:id",
-    element: <Store />,
-  },
-]);
-
 function App() {
   return (<React.StrictMode>
+      <BrowserRouter>
+        <Grid container spacing={0}>
+          <HomeAppBar />
+        </Grid>
 
-      <Grid container spacing={0}>
-        <HomeAppBar />
-      </Grid>
+        <Grid container spacing={0}  direction="column"  justifyContent="center"  alignItems="stretch">
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/store/:id" element={<Store />} />
+          </Routes>
+        </Grid>
 
-      <Grid container spacing={0}  direction="column"  justifyContent="center"  alignItems="stretch">
-        <RouterProvider router={router} />
-      </Grid>
-
-      <Grid container spacing={0} direction="row"
-        justifyContent="center"
-        alignItems="flex-end">
-        <SimpleBottomNavigation />
-      </Grid>
+        <Grid container spacing={0} direction="row"
+          justifyContent="center"
+          alignItems="flex-end">
+          <SimpleBottomNavigation />
+        </Grid>
+      </BrowserRouter>
   </React.StrictMode>);
 }
 
--- src/components/footer/index.jsx
+++ src/components/footer/index.jsx
@@ -6,9 +6,11 @@ import HomeIcon from '@mui/icons-material/Home';
 import Search from '@mui/icons-material/Search';
 import Description from '@mui/icons-material/Description';
 import Person from '@mui/icons-material/Person';
+import { useNavigate } from 'react-router-dom'
 
 export default function SimpleBottomNavigation() {
   const [value, setValue] = React.useState(0);
+  const navigate = useNavigate();
 
   return (
     <Box sx={{ position: 'fixed', bottom: 0, left: 0, right: 0 }}>
@@ -20,7 +22,7 @@ export default function SimpleBottomNavigation() {
           setValue(newValue);
         }}
       >
-        <BottomNavigationAction label="Inicio" value={"/home"} icon={<HomeIcon />} />
+        <BottomNavigationAction label="Inicio" onClick={() => navigate('/home')} value={"/home"} icon={<HomeIcon />} />
         <BottomNavigationAction label="Buscar" icon={<Search />} />
         <BottomNavigationAction label="Pedidos" icon={<Description />} />
         <BottomNavigationAction label="Perfil" icon={<Person />} />
